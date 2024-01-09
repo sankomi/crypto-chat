440336fff054fca6b996689095d128615e65ef5d
sanko <sang.koh@hotmail.com>
2024-01-09 22:27:39 +1100

    save public keys and send to new users

--- src/main/java/sanko/cryptochat/socket/SocketHandler.java
+++ src/main/java/sanko/cryptochat/socket/SocketHandler.java
@@ -1,20 +1,28 @@
 package sanko.cryptochat.socket;
 
+import java.util.Map;
 import java.io.IOException;
 import java.util.concurrent.ConcurrentHashMap;
 
 import org.springframework.stereotype.Component;
 import org.springframework.web.socket.*; //CloseStatus, TextMessage, WebSocketSession
 import org.springframework.web.socket.handler.TextWebSocketHandler;
+import org.springframework.boot.json.*; //JsonParser, JsonParserFactory
 
 @Component
 public class SocketHandler extends TextWebSocketHandler {
 
 	private static final ConcurrentHashMap<String, WebSocketSession> sockets = new ConcurrentHashMap<>();
+	private static final ConcurrentHashMap<String, String> publicKeys = new ConcurrentHashMap<>();
 
 	@Override
 	public void afterConnectionEstablished(WebSocketSession session) throws Exception {
 		sockets.put(session.getId(), session);
+		for (String username : publicKeys.keySet()) {
+			String string = "{\"type\": \"PUBLIC_KEY\", \"username\": \"" + username + "\", \"publicKey\": \"" + publicKeys.get(username).replaceAll("\n", "\\\\n") + "\"}";
+			TextMessage keyMessage = new TextMessage(string);
+			session.sendMessage(keyMessage);
+		}
 	}
 
 	@Override
@@ -24,6 +32,12 @@ public class SocketHandler extends TextWebSocketHandler {
 
 	@Override
 	protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
+		JsonParser parser = JsonParserFactory.getJsonParser();
+		Map<String, Object> json = parser.parseMap(message.getPayload());
+		if (json.get("type").equals("PUBLIC_KEY")) {
+			publicKeys.put((String) json.get("username"), (String) json.get("publicKey"));
+		}
+
 		String senderId = session.getId();
 
 		synchronized(sockets) {
